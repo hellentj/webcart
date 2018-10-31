6b616cf52097337caa5b8ce83c3bcfa8b8d47c76
hellentj <42924854+hellentj@users.noreply.github.com>
2018-10-31 16:02:07 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: 'hellenpukkacodes@gmail.com' ,
-    password: 'treesajohn123' ,
+    user_name: '' ,
+    password: '' ,
     domain: 'localhost' ,
     address: 'smtp.gmail.com',
     port: '587',
