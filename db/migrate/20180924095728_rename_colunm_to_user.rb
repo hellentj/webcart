6b72604ad8edc102fe4fd3051b538d6b26462bac
hellentj <hellentreesa@pukkacodes.com>
2018-11-02 17:07:15 +0530

    webcart commit

--- db/migrate/20180924095728_rename_colunm_to_user.rb
+++ db/migrate/20180924095728_rename_colunm_to_user.rb
@@ -1,6 +1,7 @@
 class RenameColunmToUser < ActiveRecord::Migration[5.2]
   def change
   	rename_column :users, :last_active_at, :blocked
-  	change_column :users, :blocked, 'integer USING CAST(blocked AS integer)'
+  	remove_column :users, :blocked
+  	add_column :users, :blocked, :integer
   end
 end
