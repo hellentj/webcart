ce8814ff78c0f5a05ca6fb23bf1f8689223eb8c4
hellentj <hellentreesa@pukkacodes.com>
2018-11-02 17:19:43 +0530

    webcart commit

--- db/migrate/20180924103724_rename_col_to_user.rb
+++ db/migrate/20180924103724_rename_col_to_user.rb
@@ -1,5 +1,6 @@
 class RenameColToUser < ActiveRecord::Migration[5.2]
   def change
-  	change_column :users, :blocked, :boolean, default: 0
+  	remove_column :users, :blocked
+  	add_column :users, :blocked, :boolean, default: 0
   end
 end
--- db/migrate/20180924110218_rename_column_of_user.rb
+++ db/migrate/20180924110218_rename_column_of_user.rb
@@ -1,5 +1,6 @@
 class RenameColumnOfUser < ActiveRecord::Migration[5.2]
   def change
-  	change_column :users, :blocked, :integer, default: 0
+  	remove_column :users, :blocked
+  	add_column :users, :blocked, :integer, default: 0
   end
 end
