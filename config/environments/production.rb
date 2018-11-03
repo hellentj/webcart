1f642a38b8683334021dcdfcaac1ef8352ec3706
hellentj <hellentreesa@pukkacodes.com>
2018-11-03 11:29:29 +0530

    webcart commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'myapp.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'shocking-monster-68502.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -100,7 +100,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
-    domain: "myapp.herokuapp.com",
+    domain: "shocking-monster-68502.herokuapp.com",
     authentication: :plain,
     user_name: ENV['EMAIL_ID'],
     password:  ENV['PASSWORD'],
